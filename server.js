787d25171886480a2d34dd5f6f068927580052ed
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-05-29 21:25:58 -0600

    Added most of the code

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 var express = require("express");
+var orm = require("./config/orm.js");
+
+
+orm.selectAll("burgers");
+
+orm.insertOne("example");
+
+orm.updateOne("1", "devoured")
+
+// Find a pet in the pets table by an animal_name of Rachel.
+// orm.selectWhere("pets", "animal_name", "Rachel");
+
+// // Find the buyer with the most pets.
+// orm.findWhoHasMost("buyer_name", "buyer_id", "buyers", "pets");
