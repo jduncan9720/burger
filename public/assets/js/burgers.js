85ea75b1bd7f0eca79729ebf568859d8092d7a19
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-05-31 11:14:41 -0600

    working on update code

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,24 +1,4 @@
 $(function() {
-  // $(".change-sleep").on("click", function(event) {
-  //   var id = $(this).data("id");
-  //   var newSleep = $(this).data("newsleep");
-
-  //   var newSleepState = {
-  //     sleepy: newSleep
-  //   };
-
-  //   // Send the PUT request.
-  //   $.ajax("/api/cats/" + id, {
-  //     type: "PUT",
-  //     data: newSleepState
-  //   }).then(
-  //     function() {
-  //       console.log("changed sleep to", newSleep);
-  //       // Reload the page to get the updated list
-  //       location.reload();
-  //     }
-  //   );
-  // });
 
   $(".create-form").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
@@ -41,4 +21,26 @@ $(function() {
       }
     );
   });
+
+  $("#changeDevour").on("click", function(event) {
+    var id = $(this).data("id");
+    
+    var newDevourState = {
+      devoured: 1
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevourState
+    }).then(
+      function() {
+        console.log("Burger Devoured");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  
 });
