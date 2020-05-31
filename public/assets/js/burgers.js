18ada09737b5c63a8069ec477c9912848e54d6c3
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-05-31 10:36:14 -0600

    Fixed the Add Burger button submission

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,23 +1,44 @@
-$(document).ready(function() {
+$(function() {
+  // $(".change-sleep").on("click", function(event) {
+  //   var id = $(this).data("id");
+  //   var newSleep = $(this).data("newsleep");
+
+  //   var newSleepState = {
+  //     sleepy: newSleep
+  //   };
+
+  //   // Send the PUT request.
+  //   $.ajax("/api/cats/" + id, {
+  //     type: "PUT",
+  //     data: newSleepState
+  //   }).then(
+  //     function() {
+  //       console.log("changed sleep to", newSleep);
+  //       // Reload the page to get the updated list
+  //       location.reload();
+  //     }
+  //   );
+  // });
+
   $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
 
-      var newCat = {
-        name: $("#ca").val().trim(),
-        sleepy: $("[name=sleepy]:checked").val().trim()
-      };
+    var newBurger = {
+      burger_name: $("#bu").val().trim(),
+      devoured: $("[name=devoured]:checked").val().trim()
+    };
 
-      // Send the POST request.
-      $.ajax("/api/cats", {
-        type: "POST",
-        data: newCat
-      }).then(
-        function() {
-          console.log("created new cat");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-})
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
