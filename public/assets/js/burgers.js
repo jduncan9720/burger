153f82fde128244b1841e24d991365fb27491b7b
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-06-02 18:16:23 -0600

    added Order Button and more styling

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -42,5 +42,24 @@ $(function() {
     );
   });
 
+  $(".makeburger").on("click", function(event) {
+    var id = $(this).data("id");
+    
+    var newDevourState = {
+      devoured: 0
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevourState
+    }).then(
+      function() {
+        console.log("Burger Made");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
   
 });
